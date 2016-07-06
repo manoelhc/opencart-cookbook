2265e7c1123a37767a82a0c062c59e5e4eeddc9a
Manoel Carvalho <manoelhc@gmail.com>
2016-07-06 18:44:45 -0300

    #2 Add more attributes related to admin area and URL

--- opencart/metadata.rb
+++ opencart/metadata.rb
@@ -18,6 +18,11 @@ attribute "opencart/git_url",
   :description           => "The URL of OpenCart repository to be installed",
   :default               => "https://github.com/opencart/opencart.git"
 
+attribute "opencart/url_path",
+  :display_name          => "OpenCart URL path",
+  :description           => "This is the path used after domain:port in URL. Ex.: http://local:port${url_path}",
+  :default               => "/"
+
 attribute "opencart/home_dir",
   :display_name          => "OpenCart home directory",
   :description           => "The directory of OpenCart installation",
@@ -66,4 +71,19 @@ attribute "opencart/mysql/dbname",
 attribute "opencart/mysql/table_prefix",
   :display_name          => "Table prefix",
   :description           => "This prefix will be prepended to the table's name",
+  :default               => ""
+
+attribute "opencart/admin/username",
+  :display_name          => "Admin username",
+  :description           => "Admin username",
+  :default               => "admin"
+
+attribute "opencart/admin/password",
+  :display_name          => "Admin password",
+  :description           => "Admin password",
+  :default               => "admin"
+
+attribute "opencart/admin/email",
+  :display_name          => "Admin email",
+  :description           => "Admin email",
   :default               => ""

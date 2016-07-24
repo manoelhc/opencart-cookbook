1d10c65aab89d99ce5738582e6efb71ddb8633b9
Manoel Carvalho <manoelhc@gmail.com>
2016-07-24 17:02:59 -0300

    Create the first set of definitions

---
+++ opencart/attributes/config.rb
@@ -0,0 +1,24 @@
+default[:opencart][:home_dir]     = '/opt/opencart'
+default[:opencart][:git_url]      = 'https://github.com/opencart/opencart.git'
+default[:opencart][:url_path]     = '/'
+default[:opencart][:release]      = 'master'
+default[:opencart][:http_port]    = 58080
+default[:opencart][:hostname]     = '192.168.2.110'
+default[:opencart][:webmaster_email] = 'opencart@example.com'
+
+default[:opencart][:rdbms][:name]           = 'mysqli'
+default[:opencart][:rdbms][:root_username]  = 'root'
+default[:opencart][:rdbms][:root_password]  = 'root'
+default[:opencart][:rdbms][:hostname]       = 'localhost'
+default[:opencart][:rdbms][:port]           = 3306
+default[:opencart][:rdbms][:username]       = 'opencart'
+default[:opencart][:rdbms][:password]       = 'opencart'
+default[:opencart][:rdbms][:dbname]         = 'opencart'
+default[:opencart][:rdbms][:table_prefix]   = 'oc_'
+
+default[:opencart][:admin][:username]   = 'admin'
+default[:opencart][:admin][:password]   = 'admin123'
+
+default[:opencart][:apache][:confdir]    = nil
+default[:opencart][:apache][:error_log]  = nil
+default[:opencart][:apache][:info_log]   = nil
